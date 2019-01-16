e92ecf0afb9267f6ff7bcbba3eb27e483e0ed366
alithamir <webtelecom.iq@gmail.com>
2019-01-17 00:11:23 +0300

    Update timer.cs

--- timer.cs
+++ timer.cs
@@ -1,13 +1,51 @@
- System.Timers.Timer t;
- 
-   t = new System.Timers.Timer();
-                t.Interval = 3000;
-                t.Elapsed += new System.Timers.ElapsedEventHandler(t_Elapsed);
+ using Android.App;
+using Android.Widget;
+using Android.OS;
+using System.Timers;
+using System;
+
+namespace timerandview
+{
+    [Activity(Label = "timerandview", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        int Count =1;
+        TextView txtview;
+        Timer t;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            t = new Timer();
+            t.Interval = 3000;
+            t.Elapsed += new ElapsedEventHandler(T_elp);
+
+            var bustart = FindViewById<Button>(Resource.Id.bustart);
+            var bustop = FindViewById<Button>(Resource.Id.bustop);
+            txtview = FindViewById<TextView>(Resource.Id.txtview);
+
+            bustart.Click += delegate {
                 t.Start();
 
-        protected void t_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
-        {
-            t.Stop();
-            TextView v = (TextView)FindViewById(Resource.Id.textView1);
-            v.Text = "ege";
+            };
+            bustop.Click += delegate {
+                t.Stop();
+
+            };
         }
+        protected void T_elp(object sender,ElapsedEventArgs e)
+        {
+            RunOnUiThread(() =>
+            txtview.Text = Count.ToString()
+
+                );
+            Count++;
+        }
+
+       
+       
+    }
+}
+
