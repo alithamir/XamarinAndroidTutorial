cf427563ad9c3ba13302dc1318fe3db0f301f94e
alithamir <webtelecom.iq@gmail.com>
2019-01-17 00:48:25 +0300

    Update task.cs

--- task.cs
+++ task.cs
@@ -1,22 +1,37 @@
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using System.Threading.Tasks;
+using System.Threading;
 
-//task
-var t = Task.Factory.StartNew(() => DoAction());
-
-
-
-// Thread
-//1- Check with Thread and without Thread
-ThreadPool.QueueUserWorkItem (o => SlowMethod ());
- 
- 
-
-    private void SlowMethod ()
+namespace task
+{
+    [Activity(Label = "task", MainLauncher = true)]
+    public class MainActivity : Activity
     {
-        Thread.Sleep (5000);
-        RunOnUiThread (() => textview.Text = "Method Complete");
+        Button button;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+             button = FindViewById<Button>(Resource.Id.button1);
+            button.Click += delegate {
+                var t = Task.Factory.StartNew(() => mytask());
+            };
+
+        }
+        void mytask()
+        {
+            int i = 0;
+                while (i < 10)
+            {
+                RunOnUiThread(() => button.Text = i.ToString());
+                Thread.Sleep(300);
+                i++;
+            }
+        }
     }
+}
 
-
-
-    //URL
-    //https://developer.xamarin.com/guides/android/advanced_topics/writing_responsive_applications/
